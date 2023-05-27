3b5e1a9894bbe3bbe431225dd155649491ad2e64
All <allvb@rambler.ru>
2023-05-27 20:44:32 +0300

    первый комит

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ShopItemFunc = ({item}) => {
+  return (
+    <div class="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className='description'>{item.descriptionFull}</div>
+      <div class="highlight-window mobile">
+        <div class="highlight-overlay"></div>
+      </div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">{item.currency}{item.price},00</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div> 
+  );
+}
